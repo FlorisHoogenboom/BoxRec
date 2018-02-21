072619380fbb58159ed2db6efb5ac318c913366e
Tom Rijntjes <tomrijntjes@gmail.com>
2018-02-21 13:23:37 +0100

    adding method to retrieve fights based on date

--- boxrec/sample_fights.py
+++ boxrec/sample_fights.py
@@ -1,14 +1,27 @@
 import json
 import requests
+import datetime
 from config import CS_KEY, CS_CX
 
-class CustomSearchQuery(object):
+class GenBaseClass(object):
+    """
+    Base class for custom generator objects. Expects self._generator function.
+    """
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self._generator)
+
+
+class CustomSearchQuery(GenBaseClass):
     """
     Thin Custom Search wrapper. Takes care of pagination. Max 100 results.
     """
 
-    def __init__(self,query):
+    def __init__(self,query,records=100):
         self.cs_uri = "https://www.googleapis.com/customsearch/v1"
+        self.records = records
         self.params = {
             'key':CS_KEY,
             'cx':CS_CX,
@@ -19,7 +32,7 @@ class CustomSearchQuery(object):
 
 
     def query_gen(self):
-        for page in range(1,101,10):
+        for page in range(1,self.records+1,10):
             params = self.params
             params['start']=page
             response = requests.get(self.cs_uri,params).json()
@@ -28,14 +41,27 @@ class CustomSearchQuery(object):
             for item in response['items']:
                 yield item
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return next(self._generator)
 
 
-class CollectSampleBouts(object):
+class CollectBoutsOnDate(GenBaseClass):
+    """
+    Queries fights on a certain date. Expects datetime date object in the past. Limited to 10 results.
+    """
+    def __init__(self,dt):
+        assert dt < datetime.date.today()
+        self.date = datetime.datetime.strftime(dt,"%A %m, %B %Y") #Saturday 25, November 2017
+        self._generator = self.collect_bout_uris_on_date()
+
+    def collect_bout_uris_on_date(self):
+        #adding 'scorecard judges' is a trick to prioritize bouts over events - results may vary
+        for result in CustomSearchQuery('{0} scorecard judges'.format(weight_class),records=10):
+            yield {
+                'weight': weight_class,
+                'url': result['link'],
+                'description':result['snippet']
+                }
+
+class CollectSampleBouts(GenBaseClass):
     """
     Collect a hundred fights per weight class from Google Custom query, extract the record page URI.
     """
@@ -71,14 +97,9 @@ class CollectSampleBouts(object):
                     'description':result['snippet']
                     }
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return next(self._generator)
-
 if __name__ == '__main__':
     with open('data/fight_uri.tsv','w') as f:
         for bout in CollectSampleBouts():
             print(bout)
             f.write("{0}\t{1}\n".format(bout['url'],bout['weight']))print(bout)
+"""
