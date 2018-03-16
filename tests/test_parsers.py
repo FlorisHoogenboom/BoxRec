64eb0fa57da09493a520c5893341e03169a62a08
Floris Hoogenboom <floris@digitaldreamworks.nl>
2018-03-16 13:14:10 +0100

    Added a test that draws are parsed correctly.

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,9 +1,31 @@
 import unittest
-from boxrec import models
+from boxrec.parsers import FightParser
+
+
+class MockResponse(object):
+    def __init__(self, content, encoding, url):
+        self.content= content
+        self.encoding = encoding
+        self.url = url
 
 
 class TestFightParser(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open('mock_data/fights/draw.html', 'rb') as file:
+            self.drawn_fight = file.read()
+
+        self.parser = FightParser()
+
+    def test_parses_draw(self):
+        """Test it correctly handles draws"""
+        mock_response = MockResponse(
+            self.drawn_fight,
+            'UTF-8',
+            "http://boxrec.com/en/event/115689/202488"
+        )
+
+        result = self.parser.parse(mock_response)
+        self.assertEqual(result.winner, 'drawn', "Result should equal draw.")
 
 
 class TestBoxerParser(unittest.TestCase):
